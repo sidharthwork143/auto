5a13835744120450591da31338ea91d5c710029b
sidharthwork143 <sidharthwork143@gmail.com>
2025-05-08 01:17:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import asyncio
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
